2464bbcc4a107891a6f631c575b7c3b8922b7397
jimmy <chioulf2@yahoo.com.tw>
2015-05-24 15:20:16 +0800

    鎖定那頁的網址才會執行  目前只是把每學期的課程印出來  dom確定抓的到要的資訊了  等api

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 
-	//alert(tab.url);
-
-	chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
-	    chrome.tabs.executeScript(null, { file: "content_script.js" });
-	});
+	if(tab.url == "http://moltke.cc.nccu.edu.tw/selfDevelop_SSO/lessionStudyDataList.selfDevelop"){
+		chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
+		    chrome.tabs.executeScript(null, { file: "content_script.js" });
+		});
+	}
+	else{
+		alert("不好意思 你在錯誤的頁面喔~");
+	}
 });
 
